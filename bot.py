b6cc17f1ab2d5af710019f3237e6ecedfed73c00
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-07 17:56:24 +0500

    echo

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
 from telegram import Bot
 import os
+import time
 
 TOKEN = os.environ.get('TOKEN')
 
 bot = Bot(token=TOKEN)
 
-chat_id = '1258594598'
-text = 'salom'
+last_update_id = bot.getUpdates()[-1].update_id
+while True:
+    time.sleep(1)
 
-bot.sendMessage(chat_id=chat_id, text=text)
+    curr_update = bot.getUpdates()[-1]
+
+    if last_update_id != curr_update.update_id:
+        chat_id = curr_update.message.chat.id
+        text    = curr_update.message.text
+
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+        last_update_id = curr_update.update_id
